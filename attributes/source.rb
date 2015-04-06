7549585b07d9ac366fc923c27b1a06ccd9ba54a0
Collin Grady <collin@collingrady.com>
2015-04-06 16:57:01 -0700

    changed md5 to sha1

--- attributes/source.rb
+++ attributes/source.rb
@@ -18,7 +18,7 @@
 #
 
 default['vim']['source']['version']       = '7.4'
-default['vim']['source']['checksum']      = '607e135c559be642f210094ad023dc65'
+default['vim']['source']['checksum']      = '601abf7cc2b5ab186f40d8790e542f86afca86b7'
 default['vim']['source']['prefix']        = "/usr/local"
 default['vim']['source']['configuration'] = "--without-x --enable-pythoninterp --enable-rubyinterp --enable-cscope  --with-features=huge --prefix=#{default['vim']['source']['prefix']}"
 
